28126dc0b841e5cc00b31bfc67c37a384dcd2220
sanctionedpranav <pranav9728@gmail.com>
2024-01-25 00:19:21 +0530

    channel data fetched

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
-export default ChannelDetail
+  console.log("channelDetail, videos");
+  console.log(channelDetail, videos);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => {
+      setChannelDetail(data?.items[0]);
+    });
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`).then(
+      (data) => {
+        setVideos(data?.items);
+      }
+    );
+  }, [id]);
+
+  return <div>ChannelDetail</div>;
+};
+
+export default ChannelDetail;
