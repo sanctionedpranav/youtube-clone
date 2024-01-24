ccea4e118c685482be9546f418795e139be2a256
sanctionedpranav <pranav9728@gmail.com>
2024-01-24 22:39:09 +0530

    video cards done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
   console.log("videos");
   console.log(videos);
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos?.map((item, index) => {
+        return (
+          <Box key={index}>
+            {item?.id?.videoId && <VideoCard video={item} />}
+            {item?.id?.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        );
+      })}
+    </Stack>
+  );
 };
 
 export default Videos;
